a027193c8402afed4be3ad6d234f9d5e066c4c21
Andy5948 <andy.rivera1170@hotmail.com>
2024-09-12 10:43:42 -0400

    finished crab game

--- modern-crab/CrabWorld.java
+++ modern-crab/CrabWorld.java
@@ -17,5 +17,58 @@ public class CrabWorld extends World
     public CrabWorld()
     {
         super(560, 560, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Worm worm =  new  Worm();
+        addObject(worm, 497, 128);
+        Worm worm2 =  new  Worm();
+        addObject(worm2, 428, 349);
+        Worm worm3 =  new  Worm();
+        addObject(worm3, 115, 50);
+        Worm worm4 =  new  Worm();
+        addObject(worm4, 241, 60);
+        Worm worm5 =  new  Worm();
+        addObject(worm5, 379, 36);
+        Worm worm6 =  new  Worm();
+        addObject(worm6, 68, 144);
+        Worm worm7 =  new  Worm();
+        addObject(worm7, 235, 149);
+        Worm worm8 =  new  Worm();
+        addObject(worm8, 381, 139);
+        Worm worm9 =  new  Worm();
+        addObject(worm9, 473, 235);
+        Worm worm10 =  new  Worm();
+        addObject(worm10, 312, 219);
+        Worm worm11 =  new  Worm();
+        addObject(worm11, 133, 214);
+        Worm worm12 =  new  Worm();
+        addObject(worm12, 58, 271);
+        Worm worm13 =  new  Worm();
+        addObject(worm13, 188, 273);
+        Worm worm14 =  new  Worm();
+        addObject(worm14, 355, 287);
+        Worm worm15 =  new  Worm();
+        addObject(worm15, 486, 301);
+        Worm worm16 =  new  Worm();
+        addObject(worm16, 121, 343);
+        Worm worm17 =  new  Worm();
+        addObject(worm17, 68, 430);
+        Worm worm18 =  new  Worm();
+        addObject(worm18, 186, 453);
+        Worm worm19 =  new  Worm();
+        addObject(worm19, 337, 460);
+        Worm worm20 =  new  Worm();
+        addObject(worm20, 454, 453);
+        Crab crab =  new  Crab();
+        addObject(crab, 269, 348);
+        lobster lobster =  new  lobster();
+        addObject(lobster, 275, 92);
     }
 }
